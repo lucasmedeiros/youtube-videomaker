5ce68a5ae18f77ba3960cdf13f50a9a79cbe253f
LukeHxH <lucasmed812@gmail.com>
2019-09-20 09:40:37 -0300

    Corrige script de renderização de vídeo para quaquer diretório

--- templates/default/render-video.sh
+++ templates/default/render-video.sh
@@ -2,8 +2,9 @@
 
 RENDERER=`command -v kdenlive_render`
 MELT=`command -v melt`
+DIR=`pwd`
 
-SOURCE_0="file:///home/lukehxh/Git/youtube-videomaker/templates/default/render-video.sh.mlt"
-TARGET_0="file:///home/lukehxh/Git/youtube-videomaker/templates/default/rendered-video.mp4"
+SOURCE_0="file://$DIR/templates/default/render-video.sh.mlt"
+TARGET_0="file://$DIR/templates/default/rendered-video.mp4"
 PARAMETERS_0="-pid:8248 in=0 out=1599 $MELT atsc_1080p_25 avformat - $SOURCE_0 $TARGET_0 properties=x264-medium f=mp4 vcodec=libx264 acodec=aac g=120 crf=23 ab=160k preset=faster threads=1 real_time=-1"
 $RENDERER $PARAMETERS_0
